f4cbff36c20fae716f36cbd5bf966fa66a0842f0
KwakMirae <alfo9490@naver.com>
2023-05-10 15:34:26 +0900

    feat: Spring Boot 프로젝트 세팅

---
+++ PLANU/src/main/java/com/pop/planu/PlanuApplication.java
@@ -0,0 +1,13 @@
+package com.pop.planu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanuApplication.class, args);
+    }
+
+}
