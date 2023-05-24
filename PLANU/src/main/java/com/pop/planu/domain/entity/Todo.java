88d15626ab077b11c6a315279267e2d05b7b5e5b
KwakMirae <alfo9490@naver.com>
2023-05-24 15:49:57 +0900

    feat: date 컬럼 추가(#1)

--- PLANU/src/main/java/com/pop/planu/domain/entity/Todo.java
+++ PLANU/src/main/java/com/pop/planu/domain/entity/Todo.java
@@ -3,6 +3,8 @@ package com.pop.planu.domain.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Getter
 @NoArgsConstructor
@@ -13,6 +15,9 @@ public class Todo {
     @Id @GeneratedValue(strategy = GenerationType.TABLE)
     @Column(name = "todoId")
     private Long id;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
 
     @Column(name = "title", nullable = false)
     private String title;
