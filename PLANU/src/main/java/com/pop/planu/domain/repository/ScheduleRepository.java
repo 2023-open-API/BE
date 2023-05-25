9df15ccf94753fb5317ad741468dd96286b161b6
WZNT-KimHyeJin <pcrs5278@naver.com>
2023-05-25 16:10:49 +0900

    feat: Schedule Repository

--- PLANU/src/main/java/com/pop/planu/domain/repository/ScheduleRepository.java
+++ PLANU/src/main/java/com/pop/planu/domain/repository/ScheduleRepository.java
@@ -1,4 +1,13 @@
 package com.pop.planu.domain.repository;
 
-public class ScheduleRepository {
+import com.pop.planu.domain.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
+    List<Schedule> findByAllMemberIdAndSchedleId(@Param("memberId") Long memberId, @Param("scheduleId") Long scheduleId);
 }
