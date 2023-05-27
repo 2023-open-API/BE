ff3db5c53cfcfcac075175d782d127b3a29a710b
WZNT-KimHyeJin <pcrs5278@naver.com>
2023-05-27 15:58:18 +0900

    add discription

--- PLANU/src/main/java/com/pop/planu/domain/controller/MemberRestController.java
+++ PLANU/src/main/java/com/pop/planu/domain/controller/MemberRestController.java
@@ -4,6 +4,8 @@ import com.pop.planu.domain.controller.request.member.LoginRequest;
 import com.pop.planu.domain.controller.request.member.SignUpRequest;
 import com.pop.planu.domain.service.MemberService;
 import com.pop.planu.global.dto.TokenDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,16 +20,19 @@ import static org.springframework.web.util.UriComponentsBuilder.fromPath;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
+@Tag(name = "사용자", description = "회원가입 로그인 관련 API")
 public class MemberRestController {
 
     private final MemberService memberService;
 
     @PostMapping("/login")
+    @Operation(summary = "로그인", description = "로그인 여부를 확인한다.")
     public ResponseEntity<TokenDto> login(@RequestBody LoginRequest loginRequest){
         return ResponseEntity.ok(memberService.login(loginRequest.getStudentId(),loginRequest.getPassword()));
     }
 
     @PostMapping("/signUp")
+    @Operation(summary = "회원가입", description = "회원가입 진행, 사용자 저장")
     public ResponseEntity<Void> signUp(@RequestBody SignUpRequest signUpRequest){
         Long studentId = memberService.signUp(signUpRequest);
 
--- PLANU/src/main/java/com/pop/planu/domain/controller/ScheduleRestController.java
+++ PLANU/src/main/java/com/pop/planu/domain/controller/ScheduleRestController.java
@@ -5,6 +5,8 @@ import com.pop.planu.domain.controller.request.schedule.ScheduleRequest;
 import com.pop.planu.domain.controller.response.schedule.ScheduleResponse;
 import com.pop.planu.domain.service.ScheduleService;
 import com.pop.planu.global.web.argumentresolver.Auth;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +19,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "일정관리", description = "달력 표시 일정 관련 api")
 public class ScheduleRestController {
 
     private final ScheduleService scheduleService;
 
     @PostMapping("/schedule")
+    @Operation(summary = "일정 등록", description = "새로운 일정 등록")
     public ResponseEntity<Void> registerSchedule(@Auth AuthMember authMember, @RequestBody ScheduleRequest scheduleRequest){
         Long scheduleId = scheduleService.save(scheduleRequest.toDto(authMember.getId()));
 
@@ -33,24 +37,28 @@ public class ScheduleRestController {
 
 
     @GetMapping("/schedule/month/{currentDate}")
+    @Operation(summary = "월별 일정 조회", description = "현재 일자(yyyy-mm-dd)를 기준으로 월별 일정 을 조회한다.")
     public ResponseEntity<List<ScheduleResponse>> getMonthlySchedule(@Auth AuthMember authMember, @PathVariable LocalDate currentDate){
         List<ScheduleResponse> scheduleResponseList = scheduleService.getMonthlySchedule(authMember.getId(), currentDate);
         return ResponseEntity.ok(scheduleResponseList);
     }
 
     @GetMapping("/schedule/{scheduleId}")
+    @Operation(summary = "일정 조회", description = "일정 id를 기준으로 일정을 조회한다.")
     public ResponseEntity<ScheduleResponse> getSchedule(@Auth AuthMember authMember, @PathVariable Long scheduleId){
         ScheduleResponse scheduleResponse = scheduleService.getScheduleById(scheduleId);
         return ResponseEntity.ok(scheduleResponse);
     }
 
     @PutMapping("/schedule/{scheduleId}")
+    @Operation(summary = "일정 수정", description = "일정 id를 기준으로 일정을 수정한다.")
     public ResponseEntity<ScheduleResponse> updateSchedule(@Auth AuthMember authMember,@PathVariable Long scheduleId, @RequestBody ScheduleRequest scheduleRequest){
         ScheduleResponse updateSchedule = scheduleService.update(scheduleRequest.toDto(authMember.getId(),scheduleId));
         return ResponseEntity.ok(updateSchedule);
     }
 
     @DeleteMapping("/schedule/{scheduleId}")
+    @Operation(summary = "일정 삭제", description = "일정 id를 기준으로 일정을 삭제한다.")
     public ResponseEntity<Long> deleteSchedule(@Auth AuthMember authMember,@PathVariable Long scheduleId){
         Long deleteId = scheduleService.delete(scheduleId);
         return ResponseEntity.ok(deleteId);
